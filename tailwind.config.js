113ec1bf560b788132807b392673decf95a7d4a3
jackskim <jackskim29@gmail.com>
2020-08-09 19:17:51 -0700

    Add custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#13C2C2", // Button Background, Links
+        secondary: {
+          100: "#00B696", // Footer Background
+          200: "#13C2C2", // Navbar Btn Border
+        },
+      },
+      fontFamily: {
+        heading: ["'Open Sans'"],
+        subHeading: ["'Source Sans Pro'"],
+        body: ["'Source Sans Pro'"],
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
