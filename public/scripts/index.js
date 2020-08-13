73eca4a5fa9c700783dc0efc0bb4616a3cea7a5d
jackskim <jackskim29@gmail.com>
2020-08-13 15:43:14 -0700

    Add click handlers for mobile language navigation

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,6 +3,10 @@ const menuIcon = document.querySelector("#menu-icon");
 const closeIcon = document.querySelector("#close-icon");
 const mobileNav = document.querySelector("#mobile-nav");
 
+// Mobile Language Navigation
+const languageBtn = document.querySelector("#mobile-lng-btn");
+const languageNav = document.querySelector("#mobile-lng-menu");
+
 menuIcon.addEventListener("click", () => {
   closeIcon.classList.remove("hidden");
   menuIcon.classList.add("hidden");
@@ -11,6 +15,14 @@ menuIcon.addEventListener("click", () => {
 
 closeIcon.addEventListener("click", () => {
   menuIcon.classList.remove("hidden");
+  languageBtn.classList.remove("hidden");
+  languageNav.classList.add("hidden");
   closeIcon.classList.add("hidden");
   mobileNav.classList.add("hidden");
+});
+
+languageBtn.addEventListener("click", () => {
+  languageNav.classList.remove("hidden");
+  closeIcon.classList.remove("hidden");
+  languageBtn.classList.add("hidden");
 });
